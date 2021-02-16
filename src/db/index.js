6abc55c1453c90c626e9ff61145c4d3f121a0491
Demian <devaccdemiann@gmail.com>
2021-02-16 16:14:39 +0100

    Basic mongoose

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const { db } = require("@root/config");
+const { logger } = require("../utils/logger");
+
+mongoose.set("debug", db.debug);
+
+mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(logger.info("MongoDB Connected"))
+  .catch(e => {
+    logger.error(e);
+    process.exit(0);
+  });
+
+module.exports = {};
