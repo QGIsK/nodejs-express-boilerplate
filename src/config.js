e9cde75eddd78ba7d154568d3640665aef7ca7d0
Demian <devaccdemiann@gmail.com>
2021-02-16 17:12:16 +0100

    Add selector for mongo

--- src/config.js
+++ src/config.js
@@ -6,6 +6,12 @@ exports.db = {
   debug: process.env.DB_DEBUG,
 };
 
+exports.secret = process.env.SECRET;
+
 exports.corsUrl = process.env.CORS_URL;
 
 exports.logDirectory = process.env.LOG_DIR;
+
+exports.select = {
+  user: "name email created_at updated_at",
+};
