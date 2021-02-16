bbaacbaed23ec6b9befc79fb0f4d540b38d1955b
Demian <devaccdemiann@gmail.com>
2021-02-16 16:14:14 +0100

    Basic app layout

---
+++ src/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+require('dotenv').config();
+require('module-alias/register');
+
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const handleErrors = require('@middleware/handleErrors');
+
+require('@db');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+
+app.use('/', require('./router/'));
+
+app.use(handleErrors);
+
+module.exports = app;
